249c095b86bc169f5efcd051dcf85bcdc77bfc5b
rinon2238 <fumiya.sato@valarts.space>
2021-12-08 17:30:43 +0900

    update surfaceとmodifierでテーマに沿った背景色とパディングの追加を勉強

--- app/src/main/java/space/valarts/rinon2238/jetpackcomposelessonbasic/MainActivity.kt
+++ app/src/main/java/space/valarts/rinon2238/jetpackcomposelessonbasic/MainActivity.kt
@@ -3,11 +3,14 @@ package space.valarts.rinon2238.jetpackcomposelessonbasic
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import space.valarts.rinon2238.jetpackcomposelessonbasic.ui.theme.JetpackComposeLessonBasicTheme
 
 class MainActivity : ComponentActivity() {
@@ -26,13 +29,15 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Surface(color = MaterialTheme.colors.primary) {
+        Text(text = "Hello $name!", modifier = Modifier.padding(16.dp))
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     JetpackComposeLessonBasicTheme {
-        Greeting("Android")
+        Greeting("Rinon2238")
     }
 }
