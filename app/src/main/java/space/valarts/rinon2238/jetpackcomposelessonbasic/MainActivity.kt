da5c80bc87b27168caf7667b8329dd7c2f46ea5d
rinon2238 <fumiya.sato@valarts.space>
2021-12-09 15:54:43 +0900

    update レイアウトの練習とリストで表示したViewのアニメーションについて勉強した

--- app/src/main/java/space/valarts/rinon2238/jetpackcomposelessonbasic/MainActivity.kt
+++ app/src/main/java/space/valarts/rinon2238/jetpackcomposelessonbasic/MainActivity.kt
@@ -3,17 +3,16 @@ package space.valarts.rinon2238.jetpackcomposelessonbasic
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.OutlinedButton
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -33,12 +32,25 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 private fun MyApp() {
-    val list = listOf("soy source", "oyster source")
+    var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
+
+    if (shouldShowOnboarding) {
+        OnboardingScreen {
+            shouldShowOnboarding = false
+        }
+    } else {
+        Greetings()
+    }
+}
+
+@Composable
+fun Greetings() {
+    val list: List<String> = List(1000) { "soy source. id: $it" }
 
     Surface(color = MaterialTheme.colors.background) {
-        Column {
-            list.forEach {
-                Greeting(name = it)
+        LazyColumn(modifier = Modifier.padding(bottom = 4.dp)) {
+            items(items = list) { name ->
+                Greeting(name = name)
             }
         }
     }
@@ -46,8 +58,14 @@ private fun MyApp() {
 
 @Composable
 fun Greeting(name: String) {
-    val expanded = remember { mutableStateOf(false) }
-    val extraBottomPadding = if (expanded.value) 48.dp else 0.dp
+    var expanded by rememberSaveable { mutableStateOf(false) }
+    val extraBottomPadding by animateDpAsState(
+        targetValue = if (expanded) 48.dp else 0.dp,
+        animationSpec = spring(
+            dampingRatio = Spring.DampingRatioMediumBouncy,
+            stiffness = Spring.StiffnessLow
+        )
+    )
 
     Surface(
         color = MaterialTheme.colors.primary,
@@ -59,16 +77,34 @@ fun Greeting(name: String) {
         ) {
             Column(modifier = Modifier
                 .weight(1f)
-                .padding(bottom = extraBottomPadding)
+                .padding(bottom = extraBottomPadding.coerceAtLeast(0.dp))
             ) {
                 Text(text = "This is")
                 Text(text = "$name!")
             }
 
             OutlinedButton(
-                onClick = { expanded.value = !expanded.value }
+                onClick = { expanded = !expanded }
             ) {
-                Text(text = if (expanded.value) "show less" else "show more")
+                Text(text = if (expanded) "show less" else "show more")
+            }
+        }
+    }
+}
+
+@Composable
+fun OnboardingScreen(onContinueClick: () -> Unit) {
+    Surface {
+        Column(modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Text("Welcome to the Basics Codelab!")
+            Button(
+                modifier = Modifier.padding(vertical = 24.dp),
+                onClick = onContinueClick
+            ) {
+                Text("Continue")
             }
         }
     }
@@ -78,6 +114,14 @@ fun Greeting(name: String) {
 @Composable
 fun DefaultPreview() {
     JetpackComposeLessonBasicTheme {
-        MyApp()
+        Greetings()
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+fun OnboardingPreview() {
+    JetpackComposeLessonBasicTheme {
+        OnboardingScreen {}
     }
 }
